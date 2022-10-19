5bc3ea1e0cf7fcbafd37c2c3ab51be8da5864a2f
vedant0501 <112411352+vedant0501@users.noreply.github.com>
2022-10-19 11:37:28 +0530

    HACKTOBERFEST 2022

---
+++ multiplication_VEDANT.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+cout<<"Enter Number : ";
+cin>>num;
+for(int a=1;a<=10;a++)
+{ cout<<num*a<<endl;    }
+  return 0;
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+cout<<"Enter Number : ";
+cin>>num;
+for(int a=1;a<=10;a++)
+{ cout<<num*a<<endl;    }
+  return 0;
+}
